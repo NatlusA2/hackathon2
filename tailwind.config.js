c37aaaaf15315c50828cc611c7e30513d3a7cfa9
NatlusA2 <sultanalif332@gmail.com>
2024-04-24 09:58:29 +0700

    contact and about

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#25396F',
+      },
+    },
   },
   plugins: [],
 }
